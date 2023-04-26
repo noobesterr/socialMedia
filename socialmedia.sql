aecca318c5e81a9f7125d7554a621de51eee8efd
ahmedrhouma <ahmedweldrhouma@gmail.com>
2023-04-26 18:04:02 +0100

    sql fix

--- socialmedia.sql
+++ socialmedia.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Apr 24, 2023 at 02:50 PM
+-- Generation Time: Apr 26, 2023 at 05:02 PM
 -- Server version: 8.0.30
 -- PHP Version: 8.1.10
 
@@ -28,26 +28,25 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `posts` (
-  `id` int NOT NULL,
-  `title` varchar(255) NOT NULL,
-  `description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
-  `user_id` int NOT NULL,
-  `attachments` varchar(255) DEFAULT NULL,
-  `created_at` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+                         `id` int NOT NULL,
+                         `title` varchar(255) NOT NULL,
+                         `description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
+                         `user_id` int NOT NULL,
+                         `attachments` varchar(255) DEFAULT NULL,
+                         `created_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `posts`
 --
 
 INSERT INTO `posts` (`id`, `title`, `description`, `user_id`, `attachments`, `created_at`) VALUES
-(23, 'test', 'test', 2, '[]', '2023-04-09 03:25:21'),
-(24, 'knk:jn', 'mkjnmkjn', 3, '[]', '2023-04-09 04:07:01'),
-(25, 'efmkenzvkn', 'ùùzkb rkb rebe', 2, NULL, '2023-04-23 23:50:18'),
-(26, ',v emrkjbnermkj', 'kznvmekrjber', 2, NULL, '2023-04-23 23:50:38'),
-(27, ',v emrkjbnermkj', 'kznvmekrjber', 2, NULL, '2023-04-23 23:52:08'),
-(28, 'zmkgnjvze', 'klnezerb', 2, NULL, '2023-04-23 23:52:27'),
-(29, 'zmkgnjvze', 'klnezerb', 2, NULL, '2023-04-23 23:53:17');
+                                                                                               (23, 'test', 'test', 2, '[]', '2023-04-09 03:25:21'),
+                                                                                               (24, 'knk:jn', 'mkjnmkjn', 3, '[]', '2023-04-09 04:07:01'),
+                                                                                               (26, ',v emrkjbnermkj', 'kznvmekrjber', 2, NULL, '2023-04-23 23:50:38'),
+                                                                                               (27, ',v emrkjbnermkj', 'kznvmekrjber', 2, NULL, '2023-04-23 23:52:08'),
+                                                                                               (28, 'zmkgnjvze', 'klnezerb', 2, NULL, '2023-04-23 23:52:27'),
+                                                                                               (29, 'zmkgnjvze', 'klnezerb', 2, NULL, '2023-04-23 23:53:17');
 
 -- --------------------------------------------------------
 
@@ -56,18 +55,18 @@ INSERT INTO `posts` (`id`, `title`, `description`, `user_id`, `attachments`, `cr
 --
 
 CREATE TABLE `post_reports` (
-  `id` int NOT NULL,
-  `post_id` int NOT NULL,
-  `user_id` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+                                `id` int NOT NULL,
+                                `post_id` int NOT NULL,
+                                `user_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `post_reports`
 --
 
 INSERT INTO `post_reports` (`id`, `post_id`, `user_id`) VALUES
-(13, 23, 3),
-(14, 23, 3);
+                                                            (13, 23, 3),
+                                                            (14, 23, 3);
 
 -- --------------------------------------------------------
 
@@ -76,21 +75,21 @@ INSERT INTO `post_reports` (`id`, `post_id`, `user_id`) VALUES
 --
 
 CREATE TABLE `users` (
-  `id` int NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `avatar` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `created_at` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+                         `id` int NOT NULL,
+                         `name` varchar(100) NOT NULL,
+                         `email` varchar(100) NOT NULL,
+                         `password` varchar(255) NOT NULL,
+                         `avatar` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+                         `created_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `avatar`, `created_at`) VALUES
-(2, 'ahmed', 'ahmedweldrhouma@gmail.com', '$2y$10$S8T0mQJ/l4NWv7SHavFsJegkOTbV2SR9dmcym7PkeB0TK4/iKibbm', 'uploads/avatars/2077099870.png', '2023-04-08 17:10:10'),
-(3, 'test aaaa', 'testalvnalkvnaev@gmail.com', '$2y$10$JMRtxmEC9levZrTRue1p4.a80sAvuMmNMN3/nSRGwCLyTdUEGB926', 'uploads/avatars/2115995974.PNG', '2023-04-09 02:14:43');
+                                                                                    (2, 'ahmed', 'ahmedweldrhouma@gmail.com', '$2y$10$S8T0mQJ/l4NWv7SHavFsJegkOTbV2SR9dmcym7PkeB0TK4/iKibbm', 'uploads/avatars/2077099870.png', '2023-04-08 17:10:10'),
+                                                                                    (3, 'test aaaa', 'testalvnalkvnaev@gmail.com', '$2y$10$JMRtxmEC9levZrTRue1p4.a80sAvuMmNMN3/nSRGwCLyTdUEGB926', 'uploads/avatars/2115995974.PNG', '2023-04-09 02:14:43');
 
 --
 -- Indexes for dumped tables
@@ -100,19 +99,19 @@ INSERT INTO `users` (`id`, `name`, `email`, `password`, `avatar`, `created_at`) 
 -- Indexes for table `posts`
 --
 ALTER TABLE `posts`
-  ADD PRIMARY KEY (`id`);
+    ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `post_reports`
 --
 ALTER TABLE `post_reports`
-  ADD PRIMARY KEY (`id`);
+    ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+    ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -122,19 +121,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+    MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 
 --
 -- AUTO_INCREMENT for table `post_reports`
 --
 ALTER TABLE `post_reports`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+    MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+    MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
